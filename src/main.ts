2483263bc30678090fc9e6155190282317c0734e
yusuferdem <yerdem@catchpoint.com>
2023-07-18 00:09:16 +0300

    demo

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,56 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 import * as logger from './logger'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     logger.info(`Initializing ...`)
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    logger.info(`Token ... ${core.getInput('token')}`)
+    const client = github.getOctokit(core.getInput('token'))
+    const eventName = github.context.eventName
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let base: string | undefined
+    let head: string | undefined
 
-    core.setOutput('time', new Date().toTimeString())
+    if (eventName === 'pull_request') {
+      base = github.context.payload.pull_request?.base?.sha
+      head = github.context.payload.pull_request?.head?.sha
+    } else if (eventName === 'push') {
+      base = github.context.payload.before
+      head = github.context.payload.after
+    }
+
+    if (!base || !head) {
+      logger.error('Missing data from Github.')
+
+      base = ''
+      head = ''
+    }
+
+    const response = await client.rest.repos.compareCommits({
+      base,
+      head,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
+    })
+
+    if (response.data.status !== 'ahead') {
+      logger.error(`head commit must be ahead of base commit.`)
+      return
+    }
+    const files = response.data.files
+    if (files === undefined) {
+      return
+    }
+    for (const file of files) {
+      if (file.status === 'modified') {
+        logger.info(`Modified File: ${file}`)
+      } else if (file.status === 'renamed') {
+        logger.info(`Renamed File: ${file}`)
+      } else if (file.status === 'added') {
+        logger.info(`Added File: ${file}`)
+      }
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
